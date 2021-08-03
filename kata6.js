a16983dd3316558f3036cf3c22063aa1ca18c5a2
Joe Kadi <joeekadi@gmail.com>
2021-08-03 15:11:32 +0200

    kata7 03/08/2021

--- kata6.js
+++ kata6.js
@@ -1,6 +1,5 @@
-/*
 
-You might know some pretty large perfect squares. But what about the NEXT one?
+/* You might know some pretty large perfect squares. But what about the NEXT one?
 
 Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
 
@@ -10,9 +9,9 @@ Examples:
 
 findNextSquare(121) --> returns 144
 findNextSquare(625) --> returns 676
-findNextSquare(114) --> returns -1 since 114 is not a perfect square
+findNextSquare(114) --> returns -1 since 114 is not a perfect square  */
 
-*\
+
 
 // My solution:
 
@@ -33,11 +32,8 @@ function findNextSquare(sq) {
   
 }
 
-// Best solution:
+// Most upvoted solution solution:
 
-function findNextSquare(sq) {
+function sol_findNextSquare(sq) {
   return Math.sqrt(sq)%1? -1 : Math.pow(Math.sqrt(sq)+1,2);
 }
-
-
-
