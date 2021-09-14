a6afdab39c52960c4f046f9a73f5dc822030a7ef
Joe Kadi <joekadi@Joes-MacBook-Pro-I9-2020.local>
2021-09-14 12:04:53 +0100

    kata20 14/09/21

---
+++ kata20.py
@@ -0,0 +1,16 @@
+"""
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') # returns true
+solution('abc', 'd') # returns false
+"""
+
+#My solution
+def solution(string, ending):
+    return True if string[len(string)-len(ending):len(string)] == ending else False
+
+#Most upvoted solution:
+def sol_solution(string, ending):
+    return string.endswith(ending) #lol
